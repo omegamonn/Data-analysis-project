bdd330bfdb2d3f1dbe9324955f8309c08ce517be
Đỗ Quang Vinh <57619810+QuangVinh050299@users.noreply.github.com>
2020-08-26 18:03:32 +0700

    6 (#12)
    * 6
    
    * 7

--- Project-TH/Program.cs
+++ Project-TH/Program.cs
@@ -16,7 +16,7 @@ namespace Project_TH
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new XuLyDonHang());
+            Application.Run(new PhanLoaiPhanHoi());
         }
     }
 }
