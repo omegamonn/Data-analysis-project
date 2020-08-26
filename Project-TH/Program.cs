a74aea497946336adf5b7750affbbc5f62c0a0b1
omegamonn <omega9xlast@gmail.com>
2020-08-26 15:44:48 +0700

    3

--- Project-TH/Program.cs
+++ Project-TH/Program.cs
@@ -16,7 +16,7 @@ namespace Project_TH
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new XuLyDonHang());
         }
     }
 }
